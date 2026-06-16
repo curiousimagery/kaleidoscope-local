3e2c5698c0e5dc9a033f9afcdd7607e058f58b03
Daniel Nelson <danieldnelson@gmail.com>
2026-06-16 12:33:03 -0700

    Build 167 - major refactor splitting out main.js

--- src/version.js
+++ src/version.js
@@ -17,8 +17,8 @@
 // when there's a meaningful change in surface area or behavior. don't reset
 // BUILD when bumping VERSION.
 
-export const VERSION = 'v0.9.19';
-export const BUILD = 166;
+export const VERSION = 'v0.9.20';
+export const BUILD = 167;
 
 export function formatVersion() {
   return `${VERSION} · Build ${BUILD}`;
